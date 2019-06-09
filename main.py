c2e0d3d279a5fc2a6add3f5d8e331ee741c972ab
Presac <casperhahn@hotmail.com>
2019-06-09 11:24:18 +0200

    change to acommodate data folder

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from reddit import CustomReddit
 
 def init_config():
     """Returns data from config.yaml"""
-    with open('config.yaml', 'r') as stream:
+    with open('data/config.yaml', 'r') as stream:
         try:
             config = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
@@ -17,7 +17,7 @@ def init_config():
 def init_sites_list():
     """Returns a list of sites from a yaml file."""
     sites = []
-    with open('sites.yaml', 'r') as stream:
+    with open('data/sites.yaml', 'r') as stream:
         try:
             sites = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
@@ -29,7 +29,7 @@ def init_sites_list():
 def init_redditor_list():
     """Returns a list of redditors from a yaml file."""
     redditors = []
-    with open('redditors.yaml', 'r') as stream:
+    with open('data/redditors.yaml', 'r') as stream:
         try:
             redditors = yaml.safe_load(stream)
         except yaml.YAMLError as exc:
@@ -44,7 +44,6 @@ def main():
     # Initialize information from files
     sites = init_sites_list()
     if place is 'IS_GLITCH':
-        print('IS_GLITCH')
         config = {'client_id': os.environ.get('client_id', None),
                   'client_secret': os.environ.get('client_secret', None),
                   'user_agent': os.environ.get('user_agent', None),
@@ -52,7 +51,6 @@ def main():
                   'password': os.environ.get('password', None)}
         redditors = [os.environ.get('redittors', None)]
     else:
-        print('IS_DEV')
         config = init_config()
         redditors = init_redditor_list()
 
