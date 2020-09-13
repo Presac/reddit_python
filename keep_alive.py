857d2307f1ecee00b9ce52f9461b64d7d0fde39a
Casper <casperhahn@hotmail.com>
2020-09-13 18:41:29 +0200

    Added logging

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,18 @@
 import time
 from flask import Flask
 from threading import Thread
+import logging
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print("{} UTC {:+.0f} Ping Received".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), time.localtime().tm_gmtoff / 3600))
+    logging.info(f'Ping received from {request.remote_addr}')
     return "The reddit_python bot is alive"
 
 def run():
   app.run(host='0.0.0.0',port=1717)
 
-def keep_alive():  
+def keep_alive():
     t = Thread(target=run)
     t.start()
