4016fd375b842f6cf4fcf30805f941731b731b69
Casper <casperhahn@hotmail.com>
2020-09-29 08:54:46 +0200

    Adjust to check by systemd for active state.

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,16 @@
 import time
 from flask import Flask
 from threading import Thread
+import subprocess
 
 app = Flask('')
 
 @app.route('/')
 def home():
     print("{} UTC {:+.0f} Ping Received".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()), time.localtime().tm_gmtoff / 3600))
-    return "The reddit_python bot is alive"
+    
+    status = subprocess.check_output(["systemctl", "is-active", "reddit_python.service"], universal_newlines=True).split('\n')
+    return f'The reddit_python bot is {status}'
 
 def run():
   app.run(host='0.0.0.0',port=1717)
