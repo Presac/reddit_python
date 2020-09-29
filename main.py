2d38bd75842878d85cdd051ab0042ce63afd7b13
Casper <casperhahn@hotmail.com>
2020-09-29 08:55:28 +0200

    Delete code not needed for running on raspberry.

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-import requests
 from reddit import CustomReddit
-from threading import Thread
-from keep_alive import keep_alive
 from file_import import file_importing as FI
 
 sub_name = 'FreeGameFindings'
@@ -14,30 +11,14 @@ def main():
 
     # Initialize information from files
     sites = FI.init_sites_list()
-    if place == 'IS_REPL':
-        config = {'client_id': os.environ.get('client_id', None),
-                  'client_secret': os.environ.get('client_secret', None),
-                  'user_agent': os.environ.get('user_agent', None),
-                  'username': os.environ.get('username', None),
-                  'password': os.environ.get('password', None)}
-        redditors = [os.environ.get('redditors', None)]
-    else:
-        config = FI.init_config()
-        redditors = FI.init_redditor_list()
+    config = FI.init_config()
+    redditors = FI.init_redditor_list()
 
     # Create the reddit client
     reddit = CustomReddit(config)
 
-    #if place == 'IS_REPL':
     # Start the stream for checking new posts
-    stream_thread = Thread(target=reddit.start_stream,
-                            args=(sub_name, sites, redditors),
-                            daemon=True)
-    stream_thread.start()
-
-    keep_alive()
-    #else:
-    #    reddit.start_stream(sub_name, sites, redditors)
+    reddit.start_stream(sub_name, sites, redditors)
 
 
 if __name__ == '__main__':
